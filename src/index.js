d67ff1a22770348ce9895c3b3c38abf1073fcb6f
zatuliveter <zatuliveter@gmail.com>
2022-02-15 14:18:35 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "todomvc-app-css/index.css";
+import { inspect } from "@xstate/inspect";
+import { Todos } from "./Todos";
+
+inspect({
+  iframe: false
+});
+
+ReactDOM.render(<Todos />, document.querySelector("#app"));
